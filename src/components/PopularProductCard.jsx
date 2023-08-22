ae1836a638f8725c0962b28d9e2f49e7f8c28939
Korey <shuang.lixx@gmail.com>
2023-08-22 11:02:15 +0800

    CustomerReviews

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -8,7 +8,7 @@ const PopularProductCard = ({imgURL, name, price}) => {
           <img src={star} alt="star" width={24} height={24} />
           <p className=' font-montserrat text-xl leading-normal text-slate-gray'>(4.5)</p>
         </div>
-        <h3 className='mt-2 text-2xl leading-normal font-montserrat font-palanquin'>{name}</h3>
+        <h3 className='mt-2 text-2xl leading-normal font-palanquin'>{name}</h3>
         <p className='mt-2 font-semibold font-montserrat text-coral-red leading-normal'>{price}</p>
     </div>
   )
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Button from './Button'
 import Nav from './Nav'
 import ShoeCard from './ShoeCard'
+import ReviewCard from './ReviewCard'
 
-export { Button, Nav, ShoeCard}
+export { Button, Nav, ShoeCard, ReviewCard}
